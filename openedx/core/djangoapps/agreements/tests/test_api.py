openedx/edx-platform

feat: User agreements API for generic agreement records
This change adds a new kind of generic user agreement that allows plugins or
even the core platform to record a user's acknowledgement of an agreement.

openedx/core/djangoapps/agreements/tests/test_api.py
@@ -2,25 +2,29 @@
 Tests for the Agreements API
 """
 import logging
+from datetime import datetime, timedelta
 
+from django.test import TestCase
+from opaque_keys.edx.keys import CourseKey
 from testfixtures import LogCapture
 
 from common.djangoapps.student.tests.factories import UserFactory
-from openedx.core.djangoapps.agreements.api import (
+from openedx.core.djangolib.testing.utils import skip_unless_lms
+from xmodule.modulestore.tests.django_utils import SharedModuleStoreTestCase
+from xmodule.modulestore.tests.factories import CourseFactory
+
+from ..api import (
     create_integrity_signature,
+    create_lti_pii_signature,
+    create_user_agreement_record,
     get_integrity_signature,
     get_integrity_signatures_for_course,
+    get_lti_pii_signature,
     get_pii_receiving_lti_tools,
-    create_lti_pii_signature,
-    get_lti_pii_signature
+    get_user_agreement_record,
+    get_user_agreements
 )
-from openedx.core.djangolib.testing.utils import skip_unless_lms
-from xmodule.modulestore.tests.django_utils import SharedModuleStoreTestCase  # lint-amnesty, pylint: disable=wrong-import-order
-from xmodule.modulestore.tests.factories import CourseFactory  # lint-amnesty, pylint: disable=wrong-import-order
-from ..models import (
-    LTIPIITool,
-)
-from opaque_keys.edx.keys import CourseKey
+from ..models import LTIPIITool
 
 LOGGER_NAME = "openedx.core.djangoapps.agreements.api"
 
@@ -186,3 +190,37 @@ class TestLTIPIIToolsApi(SharedModuleStoreTestCase):
         Helper function to assert the returned list has the correct tools
         """
         self.assertEqual(self.lti_tools, lti_list)
+
+
+@skip_unless_lms
+class UserAgreementsTests(TestCase):
+    """
+    Tests for the python APIs related to user agreements.
+    """
+    def setUp(self):
+        self.user = UserFactory()
+
+    def test_get_user_agreements(self, ):
+        result = list(get_user_agreements(self.user))
+        assert len(result) == 0
+
+        record = create_user_agreement_record(self.user, 'test_type')
+        result = list(get_user_agreements(self.user))
+
+        assert len(result) == 1
+        assert result[0].agreement_type == 'test_type'
+        assert result[0].username == self.user.username
+        assert result[0].accepted_at == record.accepted_at
+
+    def test_get_user_agreement_record(self):
+        record = create_user_agreement_record(self.user, 'test_type')
+        result = get_user_agreement_record(self.user, 'test_type')
+
+        assert result == record
+
+        result = get_user_agreement_record(self.user, 'test_type', datetime.now() + timedelta(days=1))
+
+        assert result is None
+
+    def tearDown(self):
+        self.user.delete()

openedx/core/djangoapps/agreements/views.py
@@ -2,21 +2,27 @@
 Views served by the Agreements app
 """
 
+import edx_api_doc_tools as apidocs
+from django import forms
 from django.conf import settings
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from drf_yasg import openapi
 from opaque_keys.edx.keys import CourseKey
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from common.djangoapps.student import auth
 from common.djangoapps.student.roles import CourseStaffRole
-from openedx.core.djangoapps.agreements.api import (
+
+from .api import (
     create_integrity_signature,
     create_lti_pii_signature,
+    create_user_agreement_record,
     get_integrity_signature,
+    get_user_agreement_record
 )
-from openedx.core.djangoapps.agreements.serializers import IntegritySignatureSerializer, LTIPIISignatureSerializer
+from .serializers import IntegritySignatureSerializer, LTIPIISignatureSerializer, UserAgreementsSerializer
 
 
 def is_user_course_or_global_staff(user, course_id):
@@ -159,3 +165,71 @@ class LTIPIISignatureView(AuthenticatedAPIView):
         else:
             statusStr = status.HTTP_500_INTERNAL_SERVER_ERROR
         return Response(data=serializer.data, status=statusStr)
+
+
+class UserAgreementsView(AuthenticatedAPIView):
+    """
+    Endpoint for the user agreements API.
+    """
+
+    class QueryFilterForm(forms.Form):
+        """
+        Query parameters for the GET method.
+        """
+        after = forms.DateTimeField(required=False)
+
+    @apidocs.schema(
+        parameters=[
+            apidocs.string_parameter(
+                'agreement_type',
+                apidocs.ParameterLocation.PATH,
+                description="Agreement ID/Type",
+            ),
+            openapi.Parameter(
+                'after',
+                apidocs.ParameterLocation.QUERY,
+                required=False,
+                type=openapi.TYPE_STRING,
+                format=openapi.FORMAT_DATETIME,
+                description="Return records after this date/time",
+            ),
+        ],
+        responses={
+            200: UserAgreementsSerializer,
+            400: "Bad Request",
+            404: "Not Found",
+        },
+    )
+    def get(self, request, agreement_type):
+        """
+        Get a user's acknowledgement record for this agreement type.
+        """
+        params = UserAgreementsView.QueryFilterForm(request.query_params)
+        if not params.is_valid():
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        record = get_user_agreement_record(request.user, agreement_type, params.cleaned_data.get('after'))
+        if record is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = UserAgreementsSerializer(record)
+        return Response(serializer.data)
+
+    @apidocs.schema(
+        parameters=[
+            apidocs.string_parameter(
+                'agreement_type',
+                apidocs.ParameterLocation.PATH,
+                description="Agreement ID/Type",
+            ),
+        ],
+        responses={
+            200: UserAgreementsSerializer,
+            400: "Bad Request",
+        },
+    )
+    def post(self, request, agreement_type):
+        """
+        Marks a user's acknowledgement of this agreement type.
+        """
+        record = create_user_agreement_record(request.user, agreement_type)
+        serializer = UserAgreementsSerializer(record)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)

open-craft/openedx-platform

(cherry picked from commit dc0383f6e38a3a7eb8e82ea1b18d0baaf66192c1)
(cherry picked from commit 2481c0e670b58df2e74a6725c3a0a6ac7bd4ac32)

openedx/core/djangoapps/agreements/tests/test_api.py
@@ -2,25 +2,29 @@
 Tests for the Agreements API
 """
 import logging
+from datetime import datetime, timedelta
 
+from django.test import TestCase
+from opaque_keys.edx.keys import CourseKey
 from testfixtures import LogCapture
 
 from common.djangoapps.student.tests.factories import UserFactory
-from openedx.core.djangoapps.agreements.api import (
+from openedx.core.djangolib.testing.utils import skip_unless_lms
+from xmodule.modulestore.tests.django_utils import SharedModuleStoreTestCase
+from xmodule.modulestore.tests.factories import CourseFactory
+
+from ..api import (
     create_integrity_signature,
+    create_lti_pii_signature,
+    create_user_agreement_record,
     get_integrity_signature,
     get_integrity_signatures_for_course,
+    get_lti_pii_signature,
     get_pii_receiving_lti_tools,
-    create_lti_pii_signature,
-    get_lti_pii_signature
+    get_latest_user_agreement_record,
+    get_user_agreements
 )
-from openedx.core.djangolib.testing.utils import skip_unless_lms
-from xmodule.modulestore.tests.django_utils import SharedModuleStoreTestCase  # lint-amnesty, pylint: disable=wrong-import-order
-from xmodule.modulestore.tests.factories import CourseFactory  # lint-amnesty, pylint: disable=wrong-import-order
-from ..models import (
-    LTIPIITool,
-)
-from opaque_keys.edx.keys import CourseKey
+from ..models import LTIPIITool
 
 LOGGER_NAME = "openedx.core.djangoapps.agreements.api"
 
@@ -186,3 +190,37 @@ class TestLTIPIIToolsApi(SharedModuleStoreTestCase):
         Helper function to assert the returned list has the correct tools
         """
         self.assertEqual(self.lti_tools, lti_list)
+
+
+@skip_unless_lms
+class UserAgreementsTests(TestCase):
+    """
+    Tests for the python APIs related to user agreements.
+    """
+    def setUp(self):
+        self.user = UserFactory()
+
+    def test_get_user_agreements(self, ):
+        result = list(get_user_agreements(self.user))
+        assert len(result) == 0
+
+        record = create_user_agreement_record(self.user, 'test_type')
+        result = list(get_user_agreements(self.user))
+
+        assert len(result) == 1
+        assert result[0].agreement_type == 'test_type'
+        assert result[0].username == self.user.username
+        assert result[0].accepted_at == record.accepted_at
+
+    def test_get_user_agreement_record(self):
+        record = create_user_agreement_record(self.user, 'test_type')
+        result = get_latest_user_agreement_record(self.user, 'test_type')
+
+        assert result == record
+
+        result = get_latest_user_agreement_record(self.user, 'test_type', datetime.now() + timedelta(days=1))
+
+        assert result is None
+
+    def tearDown(self):
+        self.user.delete()

openedx/core/djangoapps/agreements/views.py
@@ -2,21 +2,28 @@
 Views served by the Agreements app
 """
 
+import edx_api_doc_tools as apidocs
+from django import forms
 from django.conf import settings
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from drf_yasg import openapi
 from opaque_keys.edx.keys import CourseKey
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from common.djangoapps.student import auth
 from common.djangoapps.student.roles import CourseStaffRole
-from openedx.core.djangoapps.agreements.api import (
+
+from .api import (
     create_integrity_signature,
     create_lti_pii_signature,
+    create_user_agreement_record,
     get_integrity_signature,
+    get_latest_user_agreement_record
 )
-from openedx.core.djangoapps.agreements.serializers import IntegritySignatureSerializer, LTIPIISignatureSerializer
+from .serializers import IntegritySignatureSerializer, LTIPIISignatureSerializer, UserAgreementsSerializer
+from ...lib.api.view_utils import view_auth_classes
 
 
 def is_user_course_or_global_staff(user, course_id):
@@ -159,3 +166,72 @@ class LTIPIISignatureView(AuthenticatedAPIView):
         else:
             statusStr = status.HTTP_500_INTERNAL_SERVER_ERROR
         return Response(data=serializer.data, status=statusStr)
+
+
+@view_auth_classes(is_authenticated=True)
+class UserAgreementsView(APIView):
+    """
+    Endpoint for the user agreements API.
+    """
+
+    class QueryFilterForm(forms.Form):
+        """
+        Query parameters for the GET method.
+        """
+        after = forms.DateTimeField(required=False)
+
+    @apidocs.schema(
+        parameters=[
+            apidocs.string_parameter(
+                'agreement_type',
+                apidocs.ParameterLocation.PATH,
+                description="Agreement ID/Type",
+            ),
+            openapi.Parameter(
+                'after',
+                apidocs.ParameterLocation.QUERY,
+                required=False,
+                type=openapi.TYPE_STRING,
+                format=openapi.FORMAT_DATETIME,
+                description="Return records after this date/time",
+            ),
+        ],
+        responses={
+            200: UserAgreementsSerializer,
+            400: "Bad Request",
+            404: "Not Found",
+        },
+    )
+    def get(self, request, agreement_type):
+        """
+        Get a user's acknowledgement record for this agreement type.
+        """
+        params = UserAgreementsView.QueryFilterForm(request.query_params)
+        if not params.is_valid():
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        record = get_latest_user_agreement_record(request.user, agreement_type, params.cleaned_data.get('after'))
+        if record is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = UserAgreementsSerializer(record)
+        return Response(serializer.data)
+
+    @apidocs.schema(
+        parameters=[
+            apidocs.string_parameter(
+                'agreement_type',
+                apidocs.ParameterLocation.PATH,
+                description="Agreement ID/Type",
+            ),
+        ],
+        responses={
+            200: UserAgreementsSerializer,
+            400: "Bad Request",
+        },
+    )
+    def post(self, request, agreement_type):
+        """
+        Marks a user's acknowledgement of this agreement type.
+        """
+        record = create_user_agreement_record(request.user, agreement_type)
+        serializer = UserAgreementsSerializer(record)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)