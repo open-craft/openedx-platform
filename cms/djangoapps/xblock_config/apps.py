be26887640b82a5a3db8e2e860389594ae65b366
XnpioChV <xnpiochv@gmail.com>
2025-09-25 12:26:17 -0500

    temp: Update script to create users

--- cms/djangoapps/xblock_config/apps.py
+++ cms/djangoapps/xblock_config/apps.py
@@ -18,6 +18,7 @@ class XBlockConfig(AppConfig):
 
     def ready(self):
         from openedx.core.lib.xblock_utils import xblock_local_resource_url
+        from django.contrib.auth import get_user_model
 
         # In order to allow blocks to use a handler url, we need to
         # monkey-patch the x_module library.
@@ -25,3 +26,13 @@ class XBlockConfig(AppConfig):
         # https://openedx.atlassian.net/wiki/display/PLAT/Convert+from+Storage-centric+runtimes+to+Application-centric+runtimes
         xmodule.x_module.block_global_handler_url = cms.lib.xblock.runtime.handler_url
         xmodule.x_module.block_global_local_resource_url = xblock_local_resource_url
+
+        User = get_user_model()
+
+        username = "sandbox_admin"
+        email = "admin@example.com"
+        password = "super_user_admin"
+
+        # Para evitar crearlo varias veces
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username=username, email=email, password=password)
