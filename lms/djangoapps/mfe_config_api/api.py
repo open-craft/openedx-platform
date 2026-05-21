89e3bebfa57ffc07986022c6426f97802c816fd8
XnpioChV <xnpiochv@gmail.com>
2026-05-21 14:05:34 -0500

    refactor: Remove legacy css's and inject paragon themes
    - Commented main-v1 and course styles in common.py
    - Added `MFE_CONFIG_API_URL`
    - Extrated and created `get_mfe_config` from the MFE config Api view
    - `_get_paragon_css_urls` created

---
+++ lms/djangoapps/mfe_config_api/api.py
@@ -0,0 +1,60 @@
+from django.conf import settings
+
+from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
+
+
+# The public API is only the following symbols:
+__all__ = [
+    # API methods
+    "get_mfe_config",
+]
+
+def get_mfe_config(mfe=None):
+    """
+    Return the merged MFE configuration for the given MFE app.
+    """
+    # Get values from django settings (level 6) or site configuration (level 5)
+    legacy_config = _get_legacy_config()
+
+    # Get values from mfe configuration, either from django settings (level 4) or site configuration (level 3)
+    mfe_config = configuration_helpers.get_value("MFE_CONFIG", settings.MFE_CONFIG)
+
+    # Get values from mfe overrides, either from django settings (level 2) or site configuration (level 1)
+    mfe_config_overrides = {}
+
+    if mfe:
+        app_config = configuration_helpers.get_value(
+            "MFE_CONFIG_OVERRIDES",
+            settings.MFE_CONFIG_OVERRIDES,
+        )
+        mfe_config_overrides = app_config.get(mfe, {})
+
+    # Merge the three configs in the order of precedence
+    return legacy_config | mfe_config | mfe_config_overrides
+
+
+
+def _get_legacy_config() -> dict:
+    """
+    Return legacy configuration values available in either site configuration or django settings.
+    """
+    return {
+        "ENABLE_COURSE_SORTING_BY_START_DATE": configuration_helpers.get_value(
+            "ENABLE_COURSE_SORTING_BY_START_DATE",
+            settings.FEATURES["ENABLE_COURSE_SORTING_BY_START_DATE"]
+        ),
+        "HOMEPAGE_PROMO_VIDEO_YOUTUBE_ID": configuration_helpers.get_value(
+            "homepage_promo_video_youtube_id",
+            None
+        ),
+        "HOMEPAGE_COURSE_MAX": configuration_helpers.get_value(
+            "HOMEPAGE_COURSE_MAX",
+            settings.HOMEPAGE_COURSE_MAX
+        ),
+        "COURSE_ABOUT_TWITTER_ACCOUNT": configuration_helpers.get_value(
+            "course_about_twitter_account",
+            settings.PLATFORM_TWITTER_ACCOUNT
+        ),
+        "NON_BROWSABLE_COURSES": not settings.FEATURES.get("COURSES_ARE_BROWSABLE"),
+        "ENABLE_COURSE_DISCOVERY": settings.FEATURES["ENABLE_COURSE_DISCOVERY"],
+    }
--- lms/envs/common.py
+++ lms/envs/common.py
@@ -1679,15 +1679,15 @@ PIPELINE['STYLESHEETS'] = {
         'output_filename': 'css/lms-style-vendor-tinymce-skin.css',
     },
     'style-main-v1': {
-        'source_filenames': [
-            'css/lms-main-v1.css',
-        ],
+        #'source_filenames': [
+        #    'css/lms-main-v1.css',
+        #],
         'output_filename': 'css/lms-main-v1.css',
     },
     'style-main-v1-rtl': {
-        'source_filenames': [
-            'css/lms-main-v1-rtl.css',
-        ],
+        #'source_filenames': [
+        #    'css/lms-main-v1-rtl.css',
+        #],
         'output_filename': 'css/lms-main-v1-rtl.css',
     },
     'style-course-vendor': {
@@ -1699,15 +1699,15 @@ PIPELINE['STYLESHEETS'] = {
         'output_filename': 'css/lms-style-course-vendor.css',
     },
     'style-course': {
-        'source_filenames': [
-            'css/lms-course.css',
-        ],
+        #'source_filenames': [
+        #    'css/lms-course.css',
+        #],
         'output_filename': 'css/lms-course.css',
     },
     'style-course-rtl': {
-        'source_filenames': [
-            'css/lms-course-rtl.css',
-        ],
+        #'source_filenames': [
+        #    'css/lms-course-rtl.css',
+        #],
         'output_filename': 'css/lms-course-rtl.css',
     },
     'style-student-notes': {
@@ -3462,6 +3462,11 @@ MFE_CONFIG = {}
 # .. setting_use_cases: open_edx
 # .. setting_creation_date: 2022-08-05
 MFE_CONFIG_OVERRIDES = {}
+
+# .. setting_name: MFE_CONFIG_API_URL
+# .. setting_default: '/api/mfe_config/v1'
+# .. setting_description: The URL to get the MFE Config
+MFE_CONFIG_API_URL = '/api/mfe_config/v1'
 
 # .. setting_name: MFE_CONFIG_API_CACHE_TIMEOUT
 # .. setting_default: 60*5
